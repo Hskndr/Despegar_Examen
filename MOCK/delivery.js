2612b42330784cf898ff02abd6085ad4e0bdd468
hiskander aguillon <hiskander@gmail.com>
2020-01-11 19:07:34 -0300

    APP

---
+++ MOCK/delivery.js
@@ -0,0 +1,146 @@
+[
+    {
+        "id": "1",
+        "company": "Bar BQ",
+        "description": "Comida Americana, Comida Internacional, Parrilla, Ensaladas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "2",
+        "company": "Chimu",
+        "description": "Comida Peruana, Parrilla, Ensaladas, Milanesas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "3",
+        "company": "Chino Central",
+        "description": "Comida China, Ensaladas, Carnes, Pastas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "4",
+        "company": "Don Pedro",
+        "description": "Casa de empanadas, especialidades",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "5",
+        "company": "Doña Luisa",
+        "description": "Rotiseria, especialidad en minutas, milanesas, pizzas y pastas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "6",
+        "company": "El rey del Pollo",
+        "description": "Rotiseria, especialidad en pollo a la parrilla, Minutas varias",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "7",
+        "company": "Kaizen",
+        "description": "Comida Japonesa, Comida Peruana, Wick, Sushi",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "8",
+        "company": "Koh Lanta",
+        "description": "Comida Thailandesa, Carnes, Wick, Ensaladas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "9",
+        "company": "Parrilla Gonza",
+        "description": "Parrilla, Tortillas, Ensaladas, Pizza",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "10",
+        "company": "Sr Choripan",
+        "description": "Parrillas, Tartas, Hamburgeusas, Milanesas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "11",
+        "company": "Sugar Bar",
+        "description": "Comida Americana, Desayunos, Ensaladas, Hamburguesas",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+    {
+        "id": "12",
+        "company": "Suhimoto",
+        "description": "Comida Japonesa, Bebidas Alcoholicas, Sushi",
+        "categoria": {
+            "categoria-a": "pizza",
+            "categoria-b": "sandwich",
+            "categoria-c": "pastas",
+            "categoria-c": "bebidas",
+        }
+    },
+
+]
